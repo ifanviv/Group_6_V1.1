091af55401c1d8d5f58ee371cfe2258ab22b3878
ifanviv <ifan16@live.co.uk>
2015-12-06 21:30:45 +0000

    she changes like the weather

--- RevealAlgorithm.java
+++ RevealAlgorithm.java
@@ -1,11 +1,21 @@
 /**
- * Created by ifan on 02/12/2015.
+ * @file RevealAlgorithm.java
+ * @author ifan
+ * @date  02/12/2015.
+ * @see Player
+ *
+ * Reveals all tiles and will perform recursivly on tiles adjacent to mines and will reveal all mines upon hitting a mine.
  */
 public class RevealAlgorithm {
 
-
+    /**
+     *
+     * @param x where x is the x coordinate
+     * @param y where y is the y coordinate
+     */
     public static void floodFill(int x, int y) {
-        if (0 <= x && x <= Board.getBOARD_SIZE() - 1 && 0 <= y && y <= Board.getBOARD_SIZE() - 1)   {       //Boardclass.getBOARD_SIZE()
+        if (0 <= x && x <= Board.getBOARD_SIZE() - 1
+                && 0 <= y && y <= Board.getBOARD_SIZE() - 1)   {       //Boardclass.getBOARD_SIZE()
             if (Board.getTile(x, y) != null)  {
                 if (revealCheckOne(x, y)) {
                     Board.getTile(x, y).setIsHidden(false);
@@ -13,7 +23,6 @@ public class RevealAlgorithm {
                     Counter.incrementRevealedTileCount();
                     Counter.setTileRemainingCount(Board.getBOARD_SIZE());
                     System.out.println(Counter.getReveleadTileCount());
-                    //Board.getTile(x, y).changeColorRed();
                     Board.getTile(x, y).repaint();
                     if (revealCheckTwo(x, y)) {
                         floodFill(x, y + 1);
@@ -29,24 +38,44 @@ public class RevealAlgorithm {
         }
     }
 
+    /**
+     *
+     * @param x where x is the x coordinate
+     * @param y where y is the y coordinate
+     * @return
+     */
     public static Boolean revealCheckOne(int x, int y)   {
-        return (Board.getTile(x, y).getIsHidden()&& !Board.getTile(x, y).getIsMine() && !Board.getTile(x, y).getIsDiffused());
+        return (Board.getTile(x, y).getIsHidden()&& !Board.getTile(x, y).getIsMine()
+                && !Board.getTile(x, y).getIsDiffused());
     }
 
+    /**
+     *
+     * @param x where x is the x coordinate
+     * @param y where y is the y coordinate
+     * @return
+     */
     public static Boolean revealCheckTwo(int x, int y)  {
         Square square = (Square) Board.getTile(x, y);
         return (square.getNumOfMinesAdjacent() == 0);
     }
-    public void ifMine(int x, int y) {
 
+    /**
+     *
+     * @param x where x is the x coordinate
+     * @param y where y is the y coordinate
+     */
+    public void ifMine(int x, int y) {
+        if (!Board.getTile(x, y).getIsDiffused())   {
         Board.getTile(x, y).setIsHidden(false);
         for (int i = 0; i < Board.getBOARD_SIZE(); i++) {
-            for (int j = 0; j < Board.getBOARD_SIZE(); j++)  {
-                if (Board.getTile(i, j).getIsMine())    {
+            for (int j = 0; j < Board.getBOARD_SIZE(); j++) {
+                if (Board.getTile(i, j).getIsMine()) {
                     Board.getTile(i, j).setIsHidden(false);
                     Board.getTile(i, j).setEnabled(false);
                     Board.getTile(i, j).repaint();
                 }
+            }
             }
         }
     }
